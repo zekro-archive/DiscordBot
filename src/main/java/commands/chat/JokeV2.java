08a1232479d7ec9026a7a2b5fe148dac6fb499bd
zekroTJA <zekrommaster110@aol.de>
2017-06-09 10:01:23 +0200

    added joke v2 class

---
+++ src/main/java/commands/chat/JokeV2.java
@@ -0,0 +1,47 @@
+package commands.chat;
+
+import commands.Command;
+import net.dv8tion.jda.core.events.message.MessageReceivedEvent;
+import utils.STATICS;
+
+import java.io.IOException;
+import java.text.ParseException;
+
+/**
+ * Created by zekro on 09.06.2017 / 09:59
+ * DiscordBot/commands.chat
+ * © zekro 2017
+ */
+
+public class JokeV2 implements Command {
+
+    @Override
+    public boolean called(String[] args, MessageReceivedEvent event) {
+        return false;
+    }
+
+    @Override
+    public void action(String[] args, MessageReceivedEvent event) throws ParseException, IOException {
+
+    }
+
+    @Override
+    public void executed(boolean success, MessageReceivedEvent event) {
+
+    }
+
+    @Override
+    public String help() {
+        return null;
+    }
+
+    @Override
+    public String description() {
+        return null;
+    }
+
+    @Override
+    public String commandType() {
+        return STATICS.CMDTYPE.chatutils;
+    }
+}
