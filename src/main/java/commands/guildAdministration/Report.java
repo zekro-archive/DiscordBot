9607a397dff52f18a84dc1a743ddd65f375ca87e
zekroTJA <zekrommaster110@aol.de>
2017-06-12 10:44:03 +0200

    created command "report"

---
+++ src/main/java/commands/guildAdministration/Report.java
@@ -0,0 +1,50 @@
+package commands.guildAdministration;
+
+import commands.Command;
+import net.dv8tion.jda.core.events.message.MessageReceivedEvent;
+import utils.STATICS;
+
+import java.io.IOException;
+import java.text.ParseException;
+
+/**
+ * Created by zekro on 12.06.2017 / 10:38
+ * DiscordBot/commands.guildAdministration
+ * © zekro 2017
+ */
+
+public class Report implements Command {
+
+
+    @Override
+    public boolean called(String[] args, MessageReceivedEvent event) {
+        return false;
+    }
+
+    @Override
+    public void action(String[] args, MessageReceivedEvent event) throws ParseException, IOException {
+
+
+
+    }
+
+    @Override
+    public void executed(boolean success, MessageReceivedEvent event) {
+
+    }
+
+    @Override
+    public String help() {
+        return null;
+    }
+
+    @Override
+    public String description() {
+        return null;
+    }
+
+    @Override
+    public String commandType() {
+        return STATICS.CMDTYPE.guildadmin;
+    }
+}
